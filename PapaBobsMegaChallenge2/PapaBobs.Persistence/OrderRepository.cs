4df22962f78e1e17592892e896d6c40c3450859c
Gray-G <graygolden@gmail.com>
2017-05-12 18:30:06 -0700

    Commit database functionality
    Commits a Button_Click event that adds a dummy Entity object into the database. Updates the Web.config with lines from the App.config to allow Entity framework functionality.

---
+++ PapaBobsMegaChallenge2/PapaBobs.Persistence/OrderRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs.Persistence
+{
+    public class OrderRepository
+    {
+        public static void CreateOrder()
+        {
+            var db = new PapaBobsDbEntities();
+            var order = convertToEntity();
+            db.Orders.Add(order);
+            db.SaveChanges();
+        }
+
+        private static Order convertToEntity()
+        {
+            var order = new Order();
+            order.OrderId = Guid.NewGuid();
+            order.Size = DTO.Enums.SizeType.Large;
+            order.Crust = DTO.Enums.CrustType.Regular;
+            order.Name = "Test";
+            order.Address = "Test Address";
+            order.Phone = "Test Phone";
+            order.Zip = "Test zip";
+            order.PaymentType = DTO.Enums.PaymentType.Cash;
+            order.Complete = false;
+
+            return order;
+        }
+    }
+}
